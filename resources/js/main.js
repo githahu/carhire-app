12b8dd37df28a57f5305fe2110f1f47ace9b71eb
Edwin Mwangi <117833144+githahu@users.noreply.github.com>
2022-12-26 10:21:39 +0300

    Update main.js

--- resources/js/main.js
+++ resources/js/main.js
@@ -55,7 +55,18 @@ function scrollActive() {
 		}
 	});
 
-
+// comments section fucntion
+	
+	var post= document.getElementById("post");
+post.addEventListener("click", function(){
+    var commentBoxValue= document.getElementById("comment-box").value;
+ 
+    var li = document.createElement("li");
+    var text = document.createTextNode(commentBoxValue);
+    li.appendChild(text);
+    document.getElementById("unordered").appendChild(li);
+ 
+});
     
 }
 window.addEventListener("scroll", scrollActive);
